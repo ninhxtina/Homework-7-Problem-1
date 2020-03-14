47025b1bb96ee13eebe0cb90a1871fbfae65354c
ninhxtina <christinaninh07@yahoo.com>
2020-03-13 17:11:50 -0700

    added tests for remove

--- src/ArrayDictionaryTest.java
+++ src/ArrayDictionaryTest.java
@@ -16,7 +16,28 @@ public class ArrayDictionaryTest {
     @Test
     public void remove() {
         // homework
-        assertTrue(false);  // place holder
+        ArrayDictionary empty = new ArrayDictionary(0);
+        ArrayDictionary t = new ArrayDictionary(5);
+        ArrayDictionary t2 = new ArrayDictionary(4);
+
+        assertFalse(empty.remove(3)); //empty dictionary
+
+        t.add(0,10);
+        t.add(1,30);
+        t.add(2,40);
+        assertTrue(t.remove(1));
+        assertFalse(t.remove(1));
+        assertTrue(t.remove(0));
+        assertFalse(t.remove(5));
+
+        t2.add(0,12);
+        t2.add(1,15);
+        t2.add(3,37);
+        t2.add(9,20);
+        assertTrue(t2.remove(1));
+        assertTrue(t2.remove(0));
+        assertFalse(t2.remove(5));
+        assertFalse(t2.remove(11));
     }
 
     @Test
